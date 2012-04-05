31baae1fa888e93371e8847b78be688744a94a9e
Mikkel Wilson <RAMzuiv@gmail.com>
2012-04-05 15:13:08 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Apr 5, 2012
+ *      Author: RAMzuiv
+ */
+
+#include "main.h"
+
+main::main() {
+	// TODO Auto-generated constructor stub
+
+}
+
+main::~main() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ main.h
@@ -0,0 +1,17 @@
+/*
+ * main.h
+ *
+ *  Created on: Apr 5, 2012
+ *      Author: RAMzuiv
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+class main {
+public:
+	main();
+	virtual ~main();
+};
+
+#endif /* MAIN_H_ */
